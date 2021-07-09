b6ec2bc59918620208f5d88d9859449655b79416
rydem32 <87016997+rydem32@users.noreply.github.com>
2021-07-08 20:36:53 -0400

    user input!

---
+++ game.py
@@ -0,0 +1,18 @@
+# game.py
+
+print("Rock, Paper, Scissors, Shoot!")
+
+
+# ask for user input
+x = input("Please choose Rock / Paper / Scissors")
+print(x)
+
+# validate user input
+
+
+#generate a computer choice
+
+
+#determine a winner
+
+
